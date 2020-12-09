fb2d1f8a9d50ae910e66206a5dd1ba21d1223d2f
armangorjipoor <armangorjipoor@gmail.com>
2020-12-09 11:30:35 +0330

    Initial Commit

---
+++ Arman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Arman
+//
+//  Created by ArmanG on 9/19/1399 AP.
+//  Copyright © 1399 Pars Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
