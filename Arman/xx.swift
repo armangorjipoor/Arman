3ee156ff138e5590e9781604167c937446f1749f
armangorjipoor <armangorjipoor@gmail.com>
2020-12-09 12:14:53 +0330

    xx view added

---
+++ Arman/xx.swift
@@ -0,0 +1,21 @@
+//
+//  xx.swift
+//  Arman
+//
+//  Created by ArmanG on 9/19/1399 AP.
+//  Copyright © 1399 Pars Digital. All rights reserved.
+//
+
+import UIKit
+
+class xx: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
